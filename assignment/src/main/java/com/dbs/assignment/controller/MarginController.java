8283cfad7d9652b99025ed86db4ddb411c703051
Sonali Srivastava <sonali3061993@gmail.com>
2023-03-20 21:10:17 +0530

    Adding loggers

--- assignment/src/main/java/com/dbs/assignment/controller/MarginController.java
+++ assignment/src/main/java/com/dbs/assignment/controller/MarginController.java
@@ -4,6 +4,8 @@ import com.dbs.assignment.helper.CSVHelper;
 import com.dbs.assignment.model.FindMarginPayLoad;
 import com.dbs.assignment.model.Margin;
 import com.dbs.assignment.service.MarginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -16,20 +18,21 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class MarginController {
-
+    private static Logger log = LoggerFactory.getLogger(MarginController.class);
     @Autowired
     MarginService marginService;
     @PostMapping("/upload")
     public ResponseEntity<String> uploadMargins(@RequestParam("file") MultipartFile file ){
         String message = "";
+        log.debug("File type: "+file.getContentType());
         if(CSVHelper.hasCSVFormat(file)) {
-
-                marginService.save(file);
-                message = "Uploaded the file successfully: " + file.getOriginalFilename();
-                return new ResponseEntity<>(message,HttpStatus.OK);
-
+            marginService.save(file);
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            log.info("File uploaded successfully: "+file.getOriginalFilename());
+            return new ResponseEntity<>(message,HttpStatus.OK);
         }
         message = "Invalid File Format";
+        log.info("Invalid file format: "+file.getOriginalFilename());
         return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
     }
 
--- assignment/src/main/java/com/dbs/assignment/service/MarginService.java
+++ assignment/src/main/java/com/dbs/assignment/service/MarginService.java
@@ -5,6 +5,8 @@ import com.dbs.assignment.helper.CSVHelper;
 import com.dbs.assignment.model.FindMarginPayLoad;
 import com.dbs.assignment.model.Margin;
 import com.dbs.assignment.repository.MarginRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,15 +18,16 @@ import java.util.List;
 
 @Service
 public class MarginService {
-
+    private static Logger log = LoggerFactory.getLogger(MarginService.class);
     @Autowired
     MarginRepository marginRepository;
 
     public void save(MultipartFile file) {
         try {
             List<Margin> margins = CSVHelper.csvToMargin(file.getInputStream());
-
+            log.debug("---------------------Deleting entries-----------------------");
             marginRepository.deleteAll();
+            log.debug("---------------------Saving entries-----------------------");
             marginRepository.saveAll(margins);
         } catch (IOException e) {
             throw new RuntimeException("Failed to store csv data: "+e.getMessage());
@@ -36,14 +39,17 @@ public class MarginService {
         List<Margin> margins = new ArrayList<>();
         iterable.forEach(margin->margins.add(margin));
         ByteArrayInputStream result = CSVHelper.marginsToCSV(margins);
+        log.debug("Loading data: "+result.toString());
         return result;
     }
 
     public Margin findMarginByPayLoad (FindMarginPayLoad payLoad) {
         List<Margin> margins = marginRepository.findMarginByPayLoad(payLoad.getInstruction(), payLoad.getBaseCcy(), payLoad.getTermCcy(),payLoad.getTradeTier(),payLoad.getAmount());
         if(margins.size()==0) {
+            log.debug("Margin not found for payload: "+payLoad);
             throw new MarginNotFoundException("Margin Not found");
         }
+        log.debug("Returning margin: "+margins.get(0));
         return margins.get(0);
     }
 }
